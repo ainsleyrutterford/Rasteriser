d64381a8a9d2d627f9b0f6b086e8b5cc5b07d757
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-03-04 16:02:08 +0000

    Cleaned up skeletop.cpp and fixed Makefile

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -17,83 +17,69 @@ SDL_Event event;
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
 
-/* ----------------------------------------------------------------------------*/
-/* FUNCTIONS                                                                   */
+bool update();
+void draw(screen* screen);
 
-bool Update();
-void Draw(screen* screen);
+int main(int argc, char* argv[]) {
 
-int main( int argc, char* argv[] )
-{
-  
-  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
 
-  while ( Update())
-    {
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  while (update()) {
+    draw(screen);
+    SDL_Renderframe(screen);
+  }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+  SDL_SaveImage(screen, "screenshot.bmp");
 
   KillSDL(screen);
   return 0;
 }
 
-/*Place your drawing here*/
-void Draw(screen* screen)
-{
-  /* Clear buffer */
+// Place your drawing here
+void draw(screen* screen) {
+  // Clear buffer
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
+
   vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+  for(int i = 0; i < 1000; i++) {
+    uint32_t x = rand() % screen->width;
+    uint32_t y = rand() % screen->height;
+    PutPixelSDL(screen, x, y, colour);
+  }
 }
 
-/*Place updates of parameters here*/
-bool Update()
-{
-  static int t = SDL_GetTicks();
-  /* Compute frame time */
-  int t2 = SDL_GetTicks();
-  float dt = float(t2-t);
-  t = t2;
+// Place updates of parameters here
+bool update() {
+  // static int t = SDL_GetTicks();
+  // // Compute frame time
+  // int t2 = SDL_GetTicks();
+  // float dt = float(t2-t);
+  // t = t2;
 
   SDL_Event e;
-  while(SDL_PollEvent(&e))
-    {
-      if (e.type == SDL_QUIT)
-	{
-	  return false;
-	}
-      else
-	if (e.type == SDL_KEYDOWN)
-	  {
-	    int key_code = e.key.keysym.sym;
-	    switch(key_code)
-	      {
-	      case SDLK_UP:
-		/* Move camera forward */
-		break;
-	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
-	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
-	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
-	      case SDLK_ESCAPE:
-		/* Move camera quit */
-		return false;
-	      }
-	  }  
+  while(SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      return false;
+    } else if (e.type == SDL_KEYDOWN) {
+      int key_code = e.key.keysym.sym;
+      switch(key_code) {
+        case SDLK_UP:
+          // Move camera forward
+          break;
+        case SDLK_DOWN:
+          // Move camera backwards
+          break;
+        case SDLK_LEFT:
+          // Move camera left
+          break;
+        case SDLK_RIGHT:
+          // Move camera right
+          break;
+        case SDLK_ESCAPE:
+          // Move camera quit
+          return false;
+      }
     }
+  }
   return true;
 }
