aee1490b0ee612eadb919f3b0366a60171483854
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-03 13:08:06 +0100

    Finished commenting.

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -22,8 +22,8 @@ using glm::mat4;
 
 SDL_Event event;
 
-#define SCREEN_WIDTH 700
-#define SCREEN_HEIGHT 700
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
 #define FULLSCREEN_MODE false
 #define AA 2
 
@@ -271,20 +271,23 @@ void two_offscreen_x(int side, vector<Triangle>& triangles, Triangle triangle, v
              ((v0.w - 2 * v0.x / (side * SCREEN_WIDTH)) - (v1.w - 2 * v1.x / (side * SCREEN_WIDTH)));
   float t2 = 0.9999f * (v0.w - 2 * v0.x / (side * SCREEN_WIDTH)) /
              ((v0.w - 2 * v0.x / (side * SCREEN_WIDTH)) - (v2.w - 2 * v2.x / (side * SCREEN_WIDTH)));
-  // The intersection points are calculated as the start position plus ... finish me
+  // The intersection points are calculated.
   vec4 i1 = v0 + t1 * (v1 - v0);
   vec4 i2 = v0 + t2 * (v2 - v0);
+  // A new triangle is created.
   Triangle new_triangle(v0, i1, i2, triangle.color); new_triangle.normal = triangle.normal;
   triangles.push_back(new_triangle);
 }
 
 void one_offscreen_x(int side, vector<Triangle>& triangles, Triangle triangle, vec4 v0, vec4 v1, vec4 v2) {
+  // Same as above.
   float t1 = 0.9999f * (v1.w - 2 * v1.x / (side * SCREEN_WIDTH)) /
              ((v1.w - 2 * v1.x / (side * SCREEN_WIDTH)) - (v0.w - 2 * v0.x / (side * SCREEN_WIDTH)));
   float t2 = 0.9999f * (v2.w - 2 * v2.x / (side * SCREEN_WIDTH)) /
              ((v2.w - 2 * v2.x / (side * SCREEN_WIDTH)) - (v0.w - 2 * v0.x / (side * SCREEN_WIDTH)));
   vec4 i1 = v1 + t1 * (v0 - v1);
   vec4 i2 = v2 + t2 * (v0 - v2);
+  // This time two new triangles are created.
   Triangle new_triangle1(i1, v1, v2, triangle.color); new_triangle1.normal = triangle.normal;
   Triangle new_triangle2(i1, i2, v2, triangle.color); new_triangle2.normal = triangle.normal;
   triangles.push_back(new_triangle1);
@@ -315,8 +318,10 @@ void one_offscreen_y(int side, vector<Triangle>& triangles, Triangle triangle, v
   triangles.push_back(new_triangle2);
 }
 
+// Clip the top plane.
 vector<Triangle> clip_top(vector<Triangle>& triangles) {
   vector<Triangle> clipped_triangles;
+  // The max possible y.
   float y_max = SCREEN_HEIGHT / 2;
   for (uint i = 0; i < triangles.size(); i++) {
     Triangle triangle = triangles[i];
@@ -324,21 +329,28 @@ vector<Triangle> clip_top(vector<Triangle>& triangles) {
     bool v0 = true, v1 = true, v2 = true;
     bool on_screen = true;
 
+    // Check if v0 is on screen.
     if (triangle.v0.y <= -triangle.v0.w * y_max) {
       v0        = false;
       on_screen = false;
     }
+    // Check if v1 is on screen.
     if (triangle.v1.y <= -triangle.v1.w * y_max) {
       v1        = false;
       on_screen = false;
     }
+    // Check if v2 is on screen.
     if (triangle.v2.y <= -triangle.v2.w * y_max) {
       v2        = false;
       on_screen = false;
     }
 
+    // If any of them are not on screen, then dont add the triangle to the
+    // clipped triangles. If they are all on screen, add the triangle to the
+    // clipped triangles as nothing needs to be done to this triangle.
     if (on_screen) clipped_triangles.push_back(triangle);
 
+    // Now cover each possible case.
     if      ( v0 && !v1 && !v2) two_offscreen_y(-1, clipped_triangles, triangle, triangle.v0, triangle.v1, triangle.v2);
     else if (!v0 &&  v1 && !v2) two_offscreen_y(-1, clipped_triangles, triangle, triangle.v1, triangle.v0, triangle.v2);
     else if (!v0 && !v1 &&  v2) two_offscreen_y(-1, clipped_triangles, triangle, triangle.v2, triangle.v0, triangle.v1);
@@ -349,6 +361,7 @@ vector<Triangle> clip_top(vector<Triangle>& triangles) {
   return clipped_triangles;
 }
 
+// Clip the right plane. Similar to clip_top() above.
 vector<Triangle> clip_right(vector<Triangle>& triangles) {
   vector<Triangle> clipped_triangles;
   float x_max = SCREEN_WIDTH / 2;
@@ -383,6 +396,7 @@ vector<Triangle> clip_right(vector<Triangle>& triangles) {
   return clipped_triangles;
 }
 
+// Clip the bottom plane. Similar to clip_top() above.
 vector<Triangle> clip_bottom(vector<Triangle>& triangles) {
   vector<Triangle> clipped_triangles;
   float y_max = SCREEN_HEIGHT / 2;
@@ -417,6 +431,7 @@ vector<Triangle> clip_bottom(vector<Triangle>& triangles) {
   return clipped_triangles;
 }
 
+// Clip the left plane. Similar to clip_top() above.
 vector<Triangle> clip_left(vector<Triangle>& triangles) {
   vector<Triangle> clipped_triangles;
   float x_max = SCREEN_WIDTH / 2;
@@ -451,35 +466,45 @@ vector<Triangle> clip_left(vector<Triangle>& triangles) {
   return clipped_triangles;
 }
 
-void draw_polygon(screen* screen, const vector<Vertex>& vertices, vec4 current_normal, vec3 current_reflectance ) {
+// Takes three certices and draws the triangle on the screen.
+void draw_polygon(screen* screen, const vector<Vertex>& vertices, vec4 current_normal, vec3 current_reflectance) {
   uint V = vertices.size();
   vector<Pixel> vertex_pixels(V);
+  // Get the pixel for each vertex.
   for (uint i = 0; i < V; i++) {
     vertex_shader(vertices[i], vertex_pixels[i]);
   }
   vector<Pixel> left_pixels;
   vector<Pixel> right_pixels;
+  // Fill left_pixels and right_pixels with the correct values.
   compute_polygon_rows(vertex_pixels, left_pixels, right_pixels);
+  // Colour in the rows between the left_pixels and right_pixels.
   draw_rows(screen, left_pixels, right_pixels, current_normal, current_reflectance);
 }
 
+// Create a pixel given a vertex.
 void vertex_shader(const Vertex& v, Pixel& p) {
+  // Homogenisation.
   float x = focal_length * v.position.x/v.position.z + SCREEN_WIDTH /2.0f;
   float y = focal_length * v.position.y/v.position.z + SCREEN_HEIGHT/2.0f;
   float z_inv = 1.f/v.position.z;
   p = Pixel((int) x, (int) y, z_inv, v.position);
 }
 
+// Fill left_pixels and right_pixels vectors with the correct values.
 void compute_polygon_rows(const vector<Pixel>& vertex_pixels,
                           vector<Pixel>& left_pixels,
                           vector<Pixel>& right_pixels) {
   int min_y = numeric_limits<int>::max();
   int max_y = numeric_limits<int>::min();
+
+  // Find the min and max y values.
   for (uint i = 0; i < vertex_pixels.size(); i++) {
     if (vertex_pixels.at(i).y < min_y) min_y = vertex_pixels.at(i).y;
     if (vertex_pixels.at(i).y > max_y) max_y = vertex_pixels.at(i).y;
   }
 
+  // Change the left_pixels and right_pixels lengths to the correct lengths.
   left_pixels.resize(max_y - min_y + 1);
   right_pixels.resize(max_y - min_y + 1);
 
@@ -488,6 +513,8 @@ void compute_polygon_rows(const vector<Pixel>& vertex_pixels,
     right_pixels.at(i).x = numeric_limits<int>::min();
   }
 
+  // For each edge, fill the left_pixels and right_pixels vectors with the
+  // correct values.
   for (uint i = 0; i < vertex_pixels.size(); i++) {
     Pixel a = vertex_pixels.at(i);
     Pixel b = vertex_pixels.at((i + 1) % vertex_pixels.size());
@@ -498,12 +525,18 @@ void compute_polygon_rows(const vector<Pixel>& vertex_pixels,
     vector<Pixel> line(pixels);
     interpolate(a, b, line);
 
+    // For each pixel returned by interploate(), if the x value is smaller
+    // than the existing x value, then update the left pixel. If the x value
+    // is larger than the existing x value, then update the right pixel.
     for (uint j = 0; j < line.size(); j++) {
       int row = line.at(j).y - min_y;
 
+      // If new x is smaller, update left pixel.
       if (left_pixels.at(row).x > line.at(j).x) {
         left_pixels.at(row) = line.at(j);
       }
+
+      // If new x is larger, update right pixel.
       if (right_pixels.at(row).x < line.at(j).x) {
         right_pixels.at(row) = line.at(j);
       }
@@ -511,50 +544,69 @@ void compute_polygon_rows(const vector<Pixel>& vertex_pixels,
   }
 }
 
+// Given a vector of left and right pixels, call pixel_shader for each pixel
+// in between the left and right pixels.
 void draw_rows(screen* screen, const vector<Pixel>& left_pixels, const vector<Pixel>& right_pixels, vec4 current_normal, vec3 current_reflectance) {
   uint N = left_pixels.size();
 
-  // Iterate through each row
+  // Iterate through each row.
   for (uint row = 0; row < N; row++) {
 
-    // Calculate the depths of the pixels in this row
+    // Create a vector of row_pixels that is the correct length.
     vector<Pixel> row_pixels(right_pixels.at(row).x - left_pixels.at(row).x + 1);
     interpolate(left_pixels.at(row), right_pixels.at(row), row_pixels);
 
+    // For each row pixel, call pixel_shader.
     for (uint i = 0; i < row_pixels.size(); i++)  {
       pixel_shader(screen, row_pixels[i], current_normal, current_reflectance);
     }
   }
 }
 
+// Given a pixel, draw it on the correct position of the screen.
 void pixel_shader(screen* screen, const Pixel& p, vec4 current_normal, vec3 current_reflectance) {
+  // The ray from the pixel to the light.
   vec4 r = light_position - p.pos_3D;
   r.w = 0.0f;
-  float radius_sq = r.x * r.x + r.y * r.y + r.z * r.z; // WATCH OUT! Only works for w == 1
+
+  // Calculate the squared radius of the ray.
+  float radius_sq = r.x * r.x + r.y * r.y + r.z * r.z;
   vec4 n = current_normal;
 
+  // Calculate the pixel light intensity.
   vec3 D = (vec3) (light_power * (float) fmax(glm::dot(r, n) , 0)) /
                                  (float) (4 * M_PI * radius_sq);
+
+  // Multiply the intensity (plus the indirect light) by the colour.
   vec3 R = current_reflectance * (D + indirect_power_per_area);
 
   int x = p.x;
   int y = p.y;
+  // Check that the x and y are actually in the screen.
   if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+    // If this is the closest pixel so far...
     if (p.z_inv > depth_buffer[y][x]) {
+      // Update the depth_buffer.
       depth_buffer[y][x] = p.z_inv;
+      // If this pixel is a shadow pixel.
       if (current_reflectance == vec3(-1.f, -1.f, -1.f)) {
+        // Update the stencil buffer.
         stencil_buffer[y][x]++;
       } else {
+        // Otherwise, update the screen buffer with the pixel's colour.
         screen_buffer[y][x] = R;
       }
     }
   }
 }
 
+// Interpolate between two pixels.
 void interpolate(Pixel a, Pixel b, vector<Pixel>& result) {
   float steps = float(fmax(result.size() - 1, 1));
   for (uint i = 0; i < result.size(); i++) {
+    // We have a pixel class with +, -, *, and / operators in Pixel.cpp.
     result.at(i) = a + ((b - a) * ((float)i)) / steps;
+    // Perspective correct interpolation for the pos_3D.
     result[i].pos_3D = (a.pos_3D*a.z_inv + ((b.pos_3D*b.z_inv - a.pos_3D*a.z_inv) * ((float) i)) / steps) / result[i].z_inv;
     result[i].pos_3D.w = 1.f;
   }
@@ -566,49 +618,49 @@ bool update() {
   while(SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       return false;
-    } else if (e.type == SDL_MOUSEMOTION) {
+    } else if (e.type == SDL_MOUSEMOTION) { // Handle mouse movement.
       yaw   += e.motion.xrel * 0.01f;
       pitch -= e.motion.yrel * 0.01f;
-    } else if (e.type == SDL_MOUSEWHEEL) {
+    } else if (e.type == SDL_MOUSEWHEEL) {  // Hande mouse wheel input.
       focal_length += e.wheel.y;
-    } else if (e.type == SDL_KEYDOWN) {
+    } else if (e.type == SDL_KEYDOWN) {     // Handle key presses.
       int key_code = e.key.keysym.sym;
       switch(key_code) {
-        case SDLK_UP:
-          // Move camera forward
+        case SDLK_w:
+          // Move camera forward.
           camera_position.z += 0.1;
           break;
-        case SDLK_DOWN:
-          // Move camera backwards
+        case SDLK_s:
+          // Move camera backwards.
           camera_position.z -= 0.1;
           break;
+        case SDLK_a:
+          // Move camera left.
+          camera_position.x -= 0.1;
+          break;
+        case SDLK_d:
+          // Move camera right.
+          camera_position.x += 0.1;
+          break;
+        case SDLK_UP:
+          original_light_position.z += 0.2;
+          break;
+        case SDLK_DOWN:
+          original_light_position.z -= 0.2;
+          break;
+        case SDLK_LEFT:
+          original_light_position.x -= 0.2;
+          break;
+        case SDLK_RIGHT:
+          original_light_position.x += 0.2;
+          break;
         case SDLK_i:
-          // Move camera forward
+          // Move camera up.
           camera_position.y += 0.1;
           break;
         case SDLK_o:
-          // Move camera backwards
+          // Move camera down.
           camera_position.y -= 0.1;
-          break;
-        case SDLK_LEFT:
-          // Move camera left
-          camera_position.x -= 0.1;
-          break;
-        case SDLK_RIGHT:
-          // Move camera right
-          camera_position.x += 0.1;
-          break;
-        case SDLK_w:
-          original_light_position.z += 0.2;
-          break;
-        case SDLK_s:
-          original_light_position.z -= 0.2;
-          break;
-        case SDLK_a:
-          original_light_position.x -= 0.2;
-          break;
-        case SDLK_d:
-          original_light_position.x += 0.2;
           break;
         case SDLK_ESCAPE:
           // Move camera quit
